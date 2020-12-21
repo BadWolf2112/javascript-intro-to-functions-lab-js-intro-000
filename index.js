91b99ba3df42d2755841c35f482e2e248d17c563
James <jamesalexandersmith2112@gmail.com>
2020-12-21 19:07:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 } 
 function logShout(string) {
-  console.log (string.toUpperCase())
+  console.log (string.toUpperCase());
 }
 function logWhisper(string) {
-  console.log (string.toLowerCase())
+  console.log (string.toLowerCase());
 }
 function sayHiToGrandma(string) {
 
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase
-var lowercase = 'hello!'
-lowercase.toLowerCase() === lowercase
-var mixedCase = 'I love you, Grandma.'
-mixedCase.toLowerCase === mixedCase
+var uppercase = "HELLO!";
+uppercase.toUpperCase() === uppercase;
+var lowercase = 'hello!';
+lowercase.toLowerCase() === lowercase;
+var mixedCase = 'I love you, Grandma.';
+mixedCase.toLowerCase === mixedCase;
 
-mixedCase.toUpperCase === mixedCase
+mixedCase.toUpperCase === mixedCase;
 
 if (string = uppercase) {
-  return "YES INDEED!"
+  return "YES INDEED!";
 } else if (string = lowercase){
-  return "I can't hear you!"
+  return "I can't hear you!";
 } else (string = mixedCase) {
-  return "I love you, too."
+  return "I love you, too.";
 }
 }
